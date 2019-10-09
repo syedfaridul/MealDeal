c4082f0ad023c2fb21e4d823c6d0c591d6b08288
DayoBanjo <banjodayo39@gmail.com>
2019-10-09 15:18:15 -0700

    admin actvity  and setting firebase storage

--- app/src/main/java/com/example/mealdeal/foodie/AdminActivity.kt
+++ app/src/main/java/com/example/mealdeal/foodie/AdminActivity.kt
@@ -1,34 +1,88 @@
 package com.example.mealdeal.foodie
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.example.mealdeal.R
 import com.example.mealdeal.data.local.Child
+import com.example.mealdeal.util.FirestoreUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_admin.*
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class AdminActivity : AppCompatActivity() {
 
-    private lateinit var database: DatabaseReference
 
-    val childItem:Child? =null
+    private  val RC_SELECT_IMAGE = 2
+    private lateinit var database: DatabaseReference
+    private lateinit var storageReference:StorageReference
+
+    private val childItem: Child? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
 
-     initViews()
+        initViews()
     }
 
 
-    fun initViews(){
+    private fun initViews() {
 
-        button.setOnClickListener {
+        upload_button.setOnClickListener {
+
+            val intent = Intent().apply {
+                type = "image/*"
+                action = Intent.ACTION_GET_CONTENT
+                putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png"))
+            }
+            startActivityForResult(Intent.createChooser(intent, "Select Image"), RC_SELECT_IMAGE)
+        }
+
+
+        button_add_meal.setOnClickListener {
             foodDeal()
         }
     }
 
 
-    fun foodDeal(){
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SELECT_IMAGE && resultCode == Activity.RESULT_OK &&
+            data != null && data.data != null) {
+            val selectedImagePath = data.data
+
+            val selectedImageBmp = MediaStore.Images.Media.getBitmap(contentResolver, selectedImagePath)
+
+            val outputStream = ByteArrayOutputStream()
+
+            selectedImageBmp.compress(Bitmap.CompressFormat.JPEG, 90, outputStream)
+            val selectedImageBytes = outputStream.toByteArray()
+
+            storageReference = FirebaseStorage.getInstance().reference.child("deals_pictures")
+
+
+            /* StorageUtil.uploadMessageImage(selectedImageBytes) { imagePath ->
+                val messageToSend =
+                    ImageMessage(imagePath, Calendar.getInstance().time,
+                        FirebaseAuth.getInstance().currentUser!!.uid,
+                        otherUserId, currentUser.name)
+                FirestoreUtil.sendMessage(messageToSend, currentChannelId)
+            }*/
+        }
+    }
+
+
+
+
+    private fun foodDeal(){
 
         childItem!!.title=foodEditText.text.toString()
 
@@ -37,3 +91,4 @@ class AdminActivity : AppCompatActivity() {
 
     }
 }
+
