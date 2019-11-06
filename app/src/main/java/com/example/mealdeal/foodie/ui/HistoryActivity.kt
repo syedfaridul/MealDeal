ba4d319a7b845d73e51938c6c96d005d27d9a346
DayoBanjo <banjodayo39@gmail.com>
2019-11-06 08:41:31 -0800

    commented out history ui

--- app/src/main/java/com/example/mealdeal/foodie/ui/HistoryActivity.kt
+++ app/src/main/java/com/example/mealdeal/foodie/ui/HistoryActivity.kt
@@ -7,6 +7,6 @@ import com.example.mealdeal.R
 class HistoryActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+        //setContentView(R.layout.activity_history)
     }
 }
